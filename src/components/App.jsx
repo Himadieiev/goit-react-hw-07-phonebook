129e7c31e2a9f4ce375520bbdd3427e723d77e36
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-06-04 21:20:41 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,29 @@
-export const App = () => {
-  return <div></div>;
-};
+import { useSelector } from 'react-redux';
+import ContactForm from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export default function App() {
+  const filter = useSelector(state => state.filter.value);
+  const contacts = useSelector(state => state.contacts.items);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
+  return (
+    <div style={{ padding: '50px' }}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList contacts={filteredContacts} />
+    </div>
+  );
+}
